3d8aeae8a6081bebcc05f8df49f1da295918eaca
FortranVBA <mpyfabrice@gmail.com>
2021-06-03 10:33:31 +0200

    Modif front pour matcher structure wireframes

--- litreview/settings.py
+++ litreview/settings.py
@@ -143,3 +143,4 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 INTERNAL_IPS = ["127.0.0.1"]
 
 LOGIN_URL = "/account/login/"
+STATICFILES_DIRS = [(os.path.join(BASE_DIR, "litreview/static"))]
